a404fc5f5b58d17c7c1e3624d065e1ff20a61663
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-06 15:08:24 -0600

    example of windows prompt and updated index file for next example of html textbox

---
+++ 04_user_input/script.js
@@ -0,0 +1,9 @@
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
+
+
+let username = window.prompt("What's your username?");
+
+console.log(username);
