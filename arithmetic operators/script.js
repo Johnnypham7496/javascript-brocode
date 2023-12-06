5305e6695ef019e829eb49e332027f2d6725803e
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-06 14:55:41 -0600

    changed spelling error for folder and added examples of math operations in js file

---
+++ arithmetic operators/script.js
@@ -0,0 +1,30 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                   operators (+ - * /)
+//                        ex. 11 = x + 5 
+
+
+let students = 30; 
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// let extraStudents = students % 3;
+
+// console.log(extraStudents);
+
+
+// shortcuts
+// students += 1
+// students -= 1
+// students *= 1
+// students /= 1
+// students **= 1
+// students %= 1
+
+// students++;
+// students--;
+
+
+// console.log(students);
