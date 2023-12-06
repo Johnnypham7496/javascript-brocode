ecc733b4b9b9f03174ce0fe65c2bdfbba9dcbf37
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-06 14:30:28 -0600

    updated all files with necessary code for setup project and completed js with some basic examples

---
+++ tutorial for beginners/script.js
@@ -0,0 +1,18 @@
+console.log('Hello');
+console.log('I like pizza!');
+
+
+window.alert('This is an Alert!');
+window.alert('I like pizza!');
+
+
+// This is comment
+
+/*
+
+    This 
+    is 
+    a 
+    comment
+
+*/
