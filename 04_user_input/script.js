07fba0a73346447ff7c5b104320d485e1bf4c5e3
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-06 21:29:52 -0600

    updated example to use html textbox

--- 04_user_input/script.js
+++ 04_user_input/script.js
@@ -4,6 +4,13 @@
 // 2. PROFESSIONAL WAY = HTML textbox
 
 
-let username = window.prompt("What's your username?");
+// let username = window.prompt("What's your username?");
 
-console.log(username);
+// console.log(username);
+
+
+let username;
+document.getElementById("mySubmit").onclick = function() {
+    username = document.getElementById("myTextBox").value;
+    document.getElementById("myH1").textContent = `Hello ${username}`;
+}
