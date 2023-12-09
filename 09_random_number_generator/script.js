6858bcc2c81d839563f28b47bd2259a15f7a7cc7
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-08 23:01:38 -0600

    added random number logic to function when button clicked

--- 09_random_number_generator/script.js
+++ 09_random_number_generator/script.js
@@ -1,8 +1,19 @@
 // RANDOM NUMBER GENERATOR
 
-const min = 50;
-const max = 100;
+// const min = 50;
+// const max = 100;
 
-let randomNum = Math.floor(Math.random() * (max - min)) + min;
+// let randomNum = Math.floor(Math.random() * (max - min)) + min;
 
-console.log(randomNum);
+// console.log(randomNum);
+
+const myButton = document.getElementById('myButton');
+const myLabel = document.getElementById('myLabel');
+const min = 1;
+const max = 6;
+let randomNum;
+
+myButton.onclick = function() {
+    randomNum = Math.floor(Math.random() * (max - min)) + min;
+    myLabel.textContent = randomNum;
+};
