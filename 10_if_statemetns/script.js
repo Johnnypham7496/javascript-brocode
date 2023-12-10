159815a992adb1e5fdaa5610f2f3f1a718db33ff
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-10 15:56:37 -0600

    added example of time if else statement

--- 10_if_statemetns/script.js
+++ 10_if_statemetns/script.js
@@ -1,11 +1,21 @@
 // IF STATEMENTS = if a condition is true, execute some code 
 //                 if not, do something else
 
-let age = 13;
+// let age = 13;
 
-if (age>= 30) {
-    console.log("You are old enough to enter this site");
+// if (age>= 30) {
+//     console.log("You are old enough to enter this site");
+// }
+// else {
+//     console.log("You must be 18+ to enter this site");
+// }
+
+
+let time = 14;
+
+if(time < 12) {
+    console.log("Good morning!");
 }
 else {
-    console.log("You must be 18+ to enter this site");
+    console.log("Good night!");
 }
