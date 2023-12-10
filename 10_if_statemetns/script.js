87925db51e4bc6544e832c4ebf76352746e07969
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-10 16:10:52 -0600

    added example for nested if statements

--- 10_if_statemetns/script.js
+++ 10_if_statemetns/script.js
@@ -11,11 +11,28 @@
 // }
 
 
-let time = 14;
+// let time = 14;
 
-if(time < 12) {
-    console.log("Good morning!");
+// if(time < 12) {
+//     console.log("Good morning!");
+// }
+// else {
+//     console.log("Good night!");
+// }
+
+let age = 18;
+let hasLicense = false;
+
+if(age >= 16) {
+    console.log("You are old enough to drive");
+    
+    if(hasLicense){
+        console.log("You have your license!");
+    }
+    else{
+        console.log("You do not have you license yet!");
+    }
 }
-else {
-    console.log("Good night!");
+else{
+    console.log("You must be 16+ to have a license");
 }
