799978024242710e2939f46d5bed099aef5c5d1c
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-11 15:07:19 -0600

    added example of finding the last char of a string

--- 15_slicing/script.js
+++ 15_slicing/script.js
@@ -9,7 +9,9 @@ let firstName = fullName.slice(0, 2);
 let lastName = lastName.slice(7, 10);
 
 let firstChar = fullName.slice(0, 1);
+let lastChar = fullName.slice(-1);
 
 console.log(firstName);
 console.log(lastName);
 console.log(firstChar);
+console.log(lastChar);
