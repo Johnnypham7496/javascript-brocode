ac0fcc06b51a024228877762236d77d93df91bc6
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-11 15:38:36 -0600

    added final example of ! operator

--- 16_logical_operators/script.js
+++ 16_logical_operators/script.js
@@ -16,11 +16,21 @@
 // }
 
 
-const temp = -250;
+// const temp = -250;
 
-if (temp <= 0 ||temp > 30) {
-    console.log("The weather is BAD");
+// if (temp <= 0 ||temp > 30) {
+//     console.log("The weather is BAD");
+// }
+// else { 
+//     console.log("The weather is GOOD");
+// }
+
+
+const isSunny = true;
+
+if(!isSunny) {
+    console.log("The weather is CLOUDY");
 }
-else { 
-    console.log("The weather is GOOD");
+else {
+    console.log("It is SUNNY");
 }
