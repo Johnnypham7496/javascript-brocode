7ab3664962ce3e362bf4d3847f215d26fa4c1dec
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-11 14:28:58 -0600

    added different examples of using string method functions

---
+++ 14_string_methods/script.js
@@ -0,0 +1,19 @@
+// string methods = allow you to manipulate and work with text (strings)
+
+let userName = "JohnnyPham";
+
+console.log(userName.charAt(2));
+console.log(userName.indexOf("o"));
+console.log(userName.length);
+
+userName = userName.trim();
+console.log(userName);
+
+userName = userName.toUpperCase();
+console.log(userName);
+
+userName = userName.toLowerCase();
+console.log(userName);
+
+userName = userName.repeat(3);
+console.log(userName);
