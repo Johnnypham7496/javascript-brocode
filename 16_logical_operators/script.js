29c981148c951ee24e91c6c10561ec743c9d06d8
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-11 15:36:44 -0600

    added eaxmple of && || operators

---
+++ 16_logical_operators/script.js
@@ -0,0 +1,26 @@
+// logical operators = used to combine or manipulate boolean values
+//                     (true or false)
+
+//                     AND = &&
+//                     OR = ||
+//                     NOT = !
+
+
+// const temp = 200;
+
+// if (temp > 0 && temp <= 30) {
+//     console.log("The weather is GOOD");
+// }
+// else { 
+//     console.log("The weather is BAD");
+// }
+
+
+const temp = -250;
+
+if (temp <= 0 ||temp > 30) {
+    console.log("The weather is BAD");
+}
+else { 
+    console.log("The weather is GOOD");
+}
