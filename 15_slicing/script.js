b1849de1139e50f38619c2d376ee0dd3087d5e1d
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-11 15:03:46 -0600

    added example of string slicing

---
+++ 15_slicing/script.js
@@ -0,0 +1,10 @@
+// string slicing = creating a substring
+//                  from a portion of another string
+                
+//                  string.slice(start, end)
+
+
+const fullName = "Johnny Pham";
+let firstName = fullName.slice(0, 2);
+
+console.log(firstName);
