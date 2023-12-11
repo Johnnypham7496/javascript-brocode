b18cceb7db44f54b847036efffc375719db8566f
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-11 15:16:30 -0600

    final example of email slicing

--- 15_slicing/script.js
+++ 15_slicing/script.js
@@ -17,9 +17,17 @@
 // console.log(lastChar);
 
 
-const fullName = "Johnny Pham";
-let firstName = fullName.slice(0, fullName.indexOf(" "));
-let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+// const fullName = "Johnny Pham";
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1);
 
-console.log(firstName);
-console.log(lastName);
+// console.log(firstName);
+// console.log(lastName);
+
+
+const email = "Johnny1@gmail.com";
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@"));
+
+console.log(username);
+console.log(extension);
