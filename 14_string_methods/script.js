1a17feba9059e9e06cc53a4732be948b2ae4bdc2
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-11 14:39:23 -0600

    added example of includes function and added extra sacing between examples

--- 14_string_methods/script.js
+++ 14_string_methods/script.js
@@ -1,28 +1,44 @@
 // string methods = allow you to manipulate and work with text (strings)
 
-let userName = "JohnnyPham";
+let userName = "Johnny Pham";
 let result = userName.startsWith(" ");
+let resultIncludes = userName.includes(" ");
+
 
 console.log(userName.charAt(2));
 console.log(userName.indexOf("o"));
 console.log(userName.length);
 
+
 userName = userName.trim();
 console.log(userName);
+
 
 userName = userName.toUpperCase();
 console.log(userName);
 
+
 userName = userName.toLowerCase();
 console.log(userName);
+
 
 userName = userName.repeat(3);
 console.log(userName);
 
+
 console.log(result);
+
 
 if (result) {
     console.log("Your username can't begin with ' '");
+}
+else {
+    console.log(userName);
+}
+
+
+if (result) {
+    console.log("Your username can't include ' '");
 }
 else {
     console.log(userName);
