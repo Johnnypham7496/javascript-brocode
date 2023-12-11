2e85c31ead86a2b63dfe9c67980283c7659246fb
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-10 20:30:44 -0600

    added example of boolean for activity 12

--- 12_ternary_operator/script.js
+++ 12_ternary_operator/script.js
@@ -6,6 +6,10 @@
 // let message = age >= 18 ? "You're an adult" : "You're a minor";
 // console.log(message);
 
-let time = 16;
-let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
-console.log(greeting);
+// let time = 16;
+// let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+// console.log(greeting);
+
+let isStudent = true;
+let message = isStudent ? "You are a student" : "You are NOT a student";
+console.log(message);
