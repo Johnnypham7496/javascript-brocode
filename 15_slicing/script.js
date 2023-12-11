78c9aeca63842553093074947d99cfc8d5e2919b
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-11 15:13:36 -0600

    added easier example of how to slice strings and find the first and last name using indexOf function

--- 15_slicing/script.js
+++ 15_slicing/script.js
@@ -4,14 +4,22 @@
 //                  string.slice(start, end)
 
 
-const fullName = "Johnny Pham";
-let firstName = fullName.slice(0, 2);
-let lastName = lastName.slice(7, 10);
+// const fullName = "Johnny Pham";
+// let firstName = fullName.slice(0, 2);
+// let lastName = lastName.slice(7, 10);
 
-let firstChar = fullName.slice(0, 1);
-let lastChar = fullName.slice(-1);
+// let firstChar = fullName.slice(0, 1);
+// let lastChar = fullName.slice(-1);
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(firstChar);
+// console.log(lastChar);
+
+
+const fullName = "Johnny Pham";
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+let lastName = fullName.slice(fullName.indexOf(" ") + 1);
 
 console.log(firstName);
 console.log(lastName);
-console.log(firstChar);
-console.log(lastChar);
