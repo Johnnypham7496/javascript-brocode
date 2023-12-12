600656da0918738ea8acfb58b7d388a63e72c76d
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 13:08:41 -0600

    prompt user to enter a symbol to create the rectangle with

--- 21_nested_loops/script.js
+++ 21_nested_loops/script.js
@@ -1,9 +1,9 @@
 // nested loop = a loop inside of another loop
 
 
+let symbol = window.prompt("Enter a symbol to use");
 let rows = window.prompt("Enter # of rows");
 let columns = window.prompt("Enter # of columns");
-
 
 // for(let i = 1; i <= 3; i++) {
 //     for(let j = 1; j <= 3; j++) {
@@ -14,7 +14,7 @@ let columns = window.prompt("Enter # of columns");
 
 for(let i = 1; i <= rows; i++) {
     for(let j = 1; j <= columns; j++) {
-        document.getElementById("myRectangle").innerHTML += j;
+        document.getElementById("myRectangle").innerHTML += symbol;
     }
     document.getElementById("myRectangle").innerHTML += "<br>";
 }
