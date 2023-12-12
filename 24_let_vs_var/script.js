554fba0c27fa6e179f4e4209f4710776e816c7bf
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 15:54:10 -0600

    example of let variable not existing outside for loop

---
+++ 24_let_vs_var/script.js
@@ -0,0 +1,10 @@
+// variable scope = where a variable is accessible
+// let = variables are limited to block of scope {}
+// var = variables are limited to a function(){}
+
+
+ 
+for(let i = 1; i <= 3; i++) {
+    // console.log(i);
+}
+console.log(i);
