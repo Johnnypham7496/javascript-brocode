aa8b2da45fff591dbee2bb6181bdeea230587adb
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 16:33:09 -0600

    created a long string to showcase using the  and embedded variables

--- 25_template_literals/script.js
+++ 25_template_literals/script.js
@@ -12,6 +12,14 @@ let total = 75;
 // console.log("Your total is $", items);
 
 
-console.log(`Hello ${userName}`);
-console.log(`You have ${items} items in your cart`);
-console.log(`Your total is $${total}`);
+// console.log(`Hello ${userName}`);
+// console.log(`You have ${items} items in your cart`);
+// console.log(`Your total is $${total}`);
+
+
+let text = 
+`Hello ${userName} 
+You have ${items} items in your cart
+Your total is $${total}`;
+
+console.log(text);
