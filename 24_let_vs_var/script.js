c66977fee3fb491100f89f38ad9c09c7b17be806
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 16:00:42 -0600

    explanation of global variable

--- 24_let_vs_var/script.js
+++ 24_let_vs_var/script.js
@@ -1,6 +1,10 @@
 // variable scope = where a variable is accessible
+
 // let = variables are limited to block of scope {}
 // var = variables are limited to a function(){}
+
+// global variable = is declared outside any function 
+// (if global, var will CHANGE the browser's window properties)
 
 
 //  This only exists within our block and not outside meaning "i" variable doesn't exist outside our for loop
@@ -9,7 +13,7 @@
 // console.log(i);
 
 
-
+// "i" can exist outside a {} or block but it will not be accessible outside of a function block
 doSomething();
 function doSomething() {
     for(var i = 1; i <= 3; i++) {
