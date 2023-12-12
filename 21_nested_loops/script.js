6e33c6c404b0daaf914ecae8b4eeb6a3e90ce9b5
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 13:05:58 -0600

    updated js file to take in user input and create a rectangle by entering number of rows and columns

--- 21_nested_loops/script.js
+++ 21_nested_loops/script.js
@@ -1,8 +1,20 @@
 // nested loop = a loop inside of another loop
 
 
-for(let i = 1; i <= 3; i++) {
-    for(let j = 1; j <= 3; j++) {
-        console.log(j);
+let rows = window.prompt("Enter # of rows");
+let columns = window.prompt("Enter # of columns");
+
+
+// for(let i = 1; i <= 3; i++) {
+//     for(let j = 1; j <= 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+
+for(let i = 1; i <= rows; i++) {
+    for(let j = 1; j <= columns; j++) {
+        document.getElementById("myRectangle").innerHTML += j;
     }
+    document.getElementById("myRectangle").innerHTML += "<br>";
 }
