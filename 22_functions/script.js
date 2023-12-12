0ef4fc6040273f8f8932e0c80c7a7f91137aa001
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 13:16:01 -0600

    added exmaple of using functions

--- 22_functions/script.js
+++ 22_functions/script.js
@@ -2,3 +2,15 @@
 //            To preform some code, call the function name
 
 
+let userName = "Johnny";
+let age = 27;
+
+happyBirthday();
+
+function happyBirthday() {
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday dear", userName);
+    console.log("Happy birthday to you!");
+    console.log("You are", age, " years old!");
+}
