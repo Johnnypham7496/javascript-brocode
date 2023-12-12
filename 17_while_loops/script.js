c666d437431c0ba454df7acd0f8044bb7775b7c1
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 12:23:22 -0600

    added example of using a while loop to get user input

--- 17_while_loops/script.js
+++ 17_while_loops/script.js
@@ -3,3 +3,10 @@
 //              potentially infinite
 
 
+let userName = "";
+
+while (userName === "" || userName === null) {
+    userName = window.prompt("Enter your name");
+}
+
+console.log("Hello", userName);
