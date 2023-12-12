6592345adb55221be0674f67bf292dc46311c3d2
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 12:52:59 -0600

    added examples of break and continue statements

--- 20_break_and_continue_statements/script.js
+++ 20_break_and_continue_statements/script.js
@@ -1,2 +1,18 @@
 // break = breaks out of a loop entirely
 // continue = skip an iteration of a loop
+
+
+for(let i = 1; i <= 20; i++) {
+    if (i == 13) {
+        break;
+    }
+    console.log(i);
+}
+
+
+for(let i = 1; i <= 20; i++) {
+    if (i == 13) {
+        continue;
+    }
+    console.log(i);
+}
