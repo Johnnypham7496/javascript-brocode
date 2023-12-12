f27787d6d805da5acd2f0271e1cf92cd121b2e5f
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 16:17:44 -0600

    example of traditional way to write print statements

---
+++ 25_template_literals/script.js
@@ -0,0 +1,12 @@
+// Template literals = delimited with (`)
+//                     instead of double or single quotes
+//                     allows embedded variables and expressions 
+
+let userName = "Johnny";
+let items = 3;
+let total = 75;
+
+
+console.log("Hello", userName);
+console.log("You have", items, "items in your cart");
+console.log("Your total is $", items);
