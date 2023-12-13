44c7fa627a52c4fc03df98d18b1c8d6e3ab90ca0
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-12 18:43:28 -0600

    example of formatting a number to currency

--- 26_format_strings/script.js
+++ 26_format_strings/script.js
@@ -9,7 +9,15 @@
 
  let myNum = 123456.789;
 
-//  myNum = myNum.toLocaleString("en-US"); // US English
-myNum = myNum.toLocaleString("en-US");
+// myNum = myNum.toLocaleString("en-US"); // US English
+// myNum = myNum.toLocaleString("hi-IN"); // Hindi
+// myNum = myNum.toLocaleString("de-DE"); // standard German
 
- console.log(myNum);
+
+// This is how to format a number in currency
+myNum = myNum.toLocaleString("en-US", {style: "currency", currency: "USD"});
+
+
+
+
+console.log(myNum);
