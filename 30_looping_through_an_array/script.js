ad4b1e85725b3733610e94843539a9acac2dd94f
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-13 21:16:54 -0600

    example of looping through the array backwars

--- 30_looping_through_an_array/script.js
+++ 30_looping_through_an_array/script.js
@@ -3,3 +3,7 @@ let prices = [5, 10, 15, 20, 25];
 for (let i = 0; i < prices.length; i++) {
     console.log(prices[i]);
 }
+
+for (let i = prices.length - 1; i >= 0; i--) {
+    console.log(prices[i]);
+}
