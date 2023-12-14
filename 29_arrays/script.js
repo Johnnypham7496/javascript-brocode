55b41881b9ffcee0941bdd49af5638fbf03bb60b
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-13 21:12:05 -0600

    more simple examples of utilizing an array

--- 29_arrays/script.js
+++ 29_arrays/script.js
@@ -6,7 +6,17 @@ let fruits = ["apple", "orange", "banana"];
 // fruits[0] = "coconut";
 
 fruits.push("lemon"); // add an element
+fruits.pop();        // remove last element 
+fruits.unshift("mango"); //ad element to the beginning
+fruits.shift(); // removes element from the beginning
+
+
+let length = fruits.length;
+let index = fruits.indexOf("apple");
 
 console.log(fruits);
 
 console.log(fruits[0]);
+
+console.log(length);
+console.log(index);
