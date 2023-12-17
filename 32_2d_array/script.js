ff1292ce6efede53cf41f8cbbe65cc7c57cc284a
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-17 11:57:17 -0600

    example of how to replace elements in a 2D array

--- 32_2d_array/script.js
+++ 32_2d_array/script.js
@@ -7,6 +7,10 @@ let meats = ["eggs", "chicken", "fish"];
 
 let groceryList = [fruits, vegetables, meats];
 
+groceryList[0][0] = "mangoes";
+groceryList[2][0] = "steak";
+groceryList[2][2] = "steak";
+
 
 for(let list of groceryList){
     for(let food of list){
