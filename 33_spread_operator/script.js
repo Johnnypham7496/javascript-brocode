6a625d145630f40ad9e2ed26839112f9c30e8c22
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-17 12:11:35 -0600

    example of using the spread operator to get the maximum and to merge arrays

--- 33_spread_operator/script.js
+++ 33_spread_operator/script.js
@@ -12,3 +12,14 @@ console.log(numbers);
 console.log(...numbers);
 
 console.log(...userName);
+
+
+let maximum = Math.max(...numbers);
+console.log(maximum);
+
+
+let class1 = ["Spongebob", "Patrick", "Sandy"];
+let class2 = ["Squidward", "Mr.Krabs", "Plankton"];
+
+class1.push(...class2);
+console.log(...class1);
