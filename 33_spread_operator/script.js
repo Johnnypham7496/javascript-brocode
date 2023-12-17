48cc1a595289805d503ae4a21cf6b2b0687459c2
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-17 12:06:20 -0600

    exmaple of using the spread operator on an array

---
+++ 33_spread_operator/script.js
@@ -0,0 +1,9 @@
+// spread operator = allows iterable such as an
+// ...               array or string to be expanded 
+//                   in places where zero or more
+//                   arguments are expected 
+//                   (unpacks the elements)
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(numbers);
+console.log(...numbers);
