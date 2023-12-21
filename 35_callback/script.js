cda82024ef474242fad515ac917b3cc0661be807
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-20 19:10:00 -0600

    using callback function and displaying the end result in the html file

--- 35_callback/script.js
+++ 35_callback/script.js
@@ -1,3 +1,18 @@
 // callback = a function passed as an argument to another function
 
 
+let total = sum(2, 3);
+displayDOM(total);
+
+function sum(x, y) {
+    let result = x + y;
+    return result
+}
+
+function displayConsole(output) {
+    console.log(output);
+}
+
+function displayDOM(output) {
+    document.getElementById("myLabel").innerHTML = output;
+}
