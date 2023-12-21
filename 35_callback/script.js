d2692a5f72b675b6d28502960c601d7335d2cb11
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-20 19:16:02 -0600

    extra notes detailing callback functions

--- 35_callback/script.js
+++ 35_callback/script.js
@@ -1,5 +1,12 @@
 // callback = a function passed as an argument to another function
 
+// Ensures that a function is not going 
+// to run before a task is completed
+// Helps us develope asynchronous code
+// (When one function has to wait for another function)
+// that helps us avoid errors and potential problems
+// Ex. Wait for a file to load
+
 
 sum(2, 3, displayDOM);
 
