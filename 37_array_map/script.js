a3db80784cf704516ec9e805e4c9244d1bb94785
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-20 19:32:47 -0600

    example of cubing elements from our array

--- 37_array_map/script.js
+++ 37_array_map/script.js
@@ -5,11 +5,18 @@
 
 let numbers = [1, 2, 3, 4, 5];
 let squares = numbers.map(square);
+let cubes = numbers.map(cube);
 
 squares.forEach(print);
+cubes.forEach(print);
 
 function square(element) {
     return Math.pow(element, 2);
+}
+
+
+function cube(element) {
+    return Math.pow(element, 3);
 }
 
 function print(element) {
