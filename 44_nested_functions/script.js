4b9801f17919399414f6b4721da21536974471e2
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-28 18:33:06 -0600

    setup for nested function activity

---
+++ 44_nested_functions/script.js
@@ -0,0 +1,4 @@
+// nested functions = Functions inside other functions.
+//                    Outer functions have access to inner functions
+//                    Inner functions are "hidden" from the outside
+//                    used in closures (future topic)
