e932931de9a1b41e87c48912200585609b8676e2
Johnnpham7496 <johnpham7496@gmail.com>
2023-12-28 19:00:16 -0600

    completed example of using nested functions

--- 44_nested_functions/script.js
+++ 44_nested_functions/script.js
@@ -2,3 +2,22 @@
 //                    Outer functions have access to inner functions
 //                    Inner functions are "hidden" from the outside
 //                    used in closures (future topic)
+
+
+let userName = "Johnny";
+let userInbox = 0;
+
+login();
+
+function login(){
+    displayUserInbox();
+    displayUserName();
+}
+
+function displayUserName(){
+    console.log(`Welcome ${userName}`);
+}
+
+function displayUserInbox(){
+    console.log(`You have ${userInbox} new messages`);
+}
